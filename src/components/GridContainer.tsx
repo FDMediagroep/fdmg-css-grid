56362d25eb2ce79fba7c0c166cbea9da5bfcb9fe
Willem Liu <willem.liu@fdmediagroep.nl>
2020-11-02 11:05:45 +0100

    implemented getCssClassNames

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -4,107 +4,121 @@ import styles from './GridContainer.module.scss';
 /**
  * Gap attributes is only for Flex-box.
  */
-type GapAttributes =
-    | 'gap-1'
-    | 'gap-2'
-    | 'gap-3'
-    | 'gap-4'
-    | 'gap-5'
-    | 'gap-6'
-    | 'gap-7'
-    | 'gap-8'
-    | 'gap-9'
-    | 'gap-10'
-    | 'gap-11'
-    | 'gap-top'
-    | 'gap-bottom';
+enum GapDict {
+    'gap-1',
+    'gap-2',
+    'gap-3',
+    'gap-4',
+    'gap-5',
+    'gap-6',
+    'gap-7',
+    'gap-8',
+    'gap-9',
+    'gap-10',
+    'gap-11',
+    'gap-top',
+    'gap-bottom',
+}
+type GapAttributes = keyof typeof GapDict;
 
-type XSAttributes =
-    | 'xs-1'
-    | 'xs-2'
-    | 'xs-3'
-    | 'xs-4'
-    | 'xs-5'
-    | 'xs-6'
-    | 'xs-7'
-    | 'xs-8'
-    | 'xs-9'
-    | 'xs-10'
-    | 'xs-11'
-    | 'xs-12'
-    | 'xs-hide'
-    | 'xs-show';
+enum XSDict {
+    'xs-1',
+    'xs-2',
+    'xs-3',
+    'xs-4',
+    'xs-5',
+    'xs-6',
+    'xs-7',
+    'xs-8',
+    'xs-9',
+    'xs-10',
+    'xs-11',
+    'xs-12',
+    'xs-hide',
+    'xs-show',
+}
+type XSAttributes = keyof typeof XSDict;
 
-type SAttributes =
-    | 's-1'
-    | 's-2'
-    | 's-3'
-    | 's-4'
-    | 's-5'
-    | 's-6'
-    | 's-7'
-    | 's-8'
-    | 's-9'
-    | 's-10'
-    | 's-11'
-    | 's-12'
-    | 's-hide'
-    | 's-show';
+enum SDict {
+    's-1',
+    's-2',
+    's-3',
+    's-4',
+    's-5',
+    's-6',
+    's-7',
+    's-8',
+    's-9',
+    's-10',
+    's-11',
+    's-12',
+    's-hide',
+    's-show',
+}
+type SAttributes = keyof typeof SDict;
 
-type MAttributes =
-    | 'm-1'
-    | 'm-2'
-    | 'm-3'
-    | 'm-4'
-    | 'm-5'
-    | 'm-6'
-    | 'm-7'
-    | 'm-8'
-    | 'm-9'
-    | 'm-10'
-    | 'm-11'
-    | 'm-12'
-    | 'm-hide'
-    | 'm-show';
+enum MDict {
+    'm-1',
+    'm-2',
+    'm-3',
+    'm-4',
+    'm-5',
+    'm-6',
+    'm-7',
+    'm-8',
+    'm-9',
+    'm-10',
+    'm-11',
+    'm-12',
+    'm-hide',
+    'm-show',
+}
+type MAttributes = keyof typeof MDict;
 
-type LAttributes =
-    | 'l-1'
-    | 'l-2'
-    | 'l-3'
-    | 'l-4'
-    | 'l-5'
-    | 'l-6'
-    | 'l-7'
-    | 'l-8'
-    | 'l-9'
-    | 'l-10'
-    | 'l-11'
-    | 'l-12'
-    | 'l-hide'
-    | 'l-show';
+enum LDict {
+    'l-1',
+    'l-2',
+    'l-3',
+    'l-4',
+    'l-5',
+    'l-6',
+    'l-7',
+    'l-8',
+    'l-9',
+    'l-10',
+    'l-11',
+    'l-12',
+    'l-hide',
+    'l-show',
+}
+type LAttributes = keyof typeof LDict;
 
-type XLAttributes =
-    | 'xl-1'
-    | 'xl-2'
-    | 'xl-3'
-    | 'xl-4'
-    | 'xl-5'
-    | 'xl-6'
-    | 'xl-7'
-    | 'xl-8'
-    | 'xl-9'
-    | 'xl-10'
-    | 'xl-11'
-    | 'xl-12'
-    | 'xl-hide'
-    | 'xl-show';
+enum XLDict {
+    'xl-1',
+    'xl-2',
+    'xl-3',
+    'xl-4',
+    'xl-5',
+    'xl-6',
+    'xl-7',
+    'xl-8',
+    'xl-9',
+    'xl-10',
+    'xl-11',
+    'xl-12',
+    'xl-hide',
+    'xl-show',
+}
+type XLAttributes = keyof typeof XLDict;
 
-type HideAttributes =
-    | 'hide-lt-xs'
-    | 'hide-lt-s'
-    | 'hide-lt-m'
-    | 'hide-lt-l'
-    | 'hide-lt-xl';
+enum HideDict {
+    'hide-lt-xs',
+    'hide-lt-s',
+    'hide-lt-m',
+    'hide-lt-l',
+    'hide-lt-xl',
+}
+type HideAttributes = keyof typeof HideDict;
 
 interface Props {
     attributes: (
@@ -121,7 +135,7 @@ interface Props {
     [x: string]: any;
 }
 
-export function GridContainer(props: Props) {
+function GridContainer(props: Props) {
     return (
         <div
             className={`${props?.className ?? ''}${
@@ -143,3 +157,18 @@ export function GridContainer(props: Props) {
         </div>
     );
 }
+
+function getCssClassNames(): string[] {
+    return [
+        styles.content,
+        ...Object.keys(GapDict),
+        ...Object.keys(XSDict),
+        ...Object.keys(SDict),
+        ...Object.keys(MDict),
+        ...Object.keys(LDict),
+        ...Object.keys(XLDict),
+        ...Object.keys(HideDict),
+    ];
+}
+
+export { GridContainer, getCssClassNames };
