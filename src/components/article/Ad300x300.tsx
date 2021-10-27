1a164fdf584f7d5da036cab6c7dd9ca6e6b0cd7e
Willem Liu <willem_liu@hotmail.com>
2021-10-27 09:21:26 +0200

    next@12

--- src/components/article/Ad300x300.tsx
+++ src/components/article/Ad300x300.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Ad300x300(props: any) {
     return (
         <div className={`${props.className ?? ''} sticky dummy-ad-300`}>
-            <img src="/assets/images/advertorialAd.png" />
+            <img src="/assets/images/advertorialAd.png" alt="ad" />
         </div>
     );
 }
