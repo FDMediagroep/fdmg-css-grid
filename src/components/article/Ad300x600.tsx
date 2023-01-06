6848e0749f34bea0973068132c6ccc262b3af9ca
Willem Liu <willem.liu@fdmediagroep.nl>
2023-01-06 09:24:27 +0100

    Fixed some linting warnings

--- src/components/article/Ad300x600.tsx
+++ src/components/article/Ad300x600.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Ad300x600(props: any) {
     return (
         <div className={`${props.className ?? ''} sticky dummy-ad-300`}>
-            <img src="/assets/images/ad.png" />
+            <img src="/assets/images/ad.png" alt="ad" />
         </div>
     );
 }
